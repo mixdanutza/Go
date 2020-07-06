d173f154a504f7ecaafae049e8dd6f4f3614eff7
mixdanutza <bizgudaniela1@gmail.com>
2020-07-06 12:04:52 +0300

    Interfaces basics

---
+++ interfaces/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type bot interface {
+	getGreeting() string
+}
+
+type englishBot struct{}
+
+type spanishBot struct{}
+
+func main() {
+
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(sb)
+	printGreeting(eb)
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func (englishBot) getGreeting() string {
+	return "Hey there!"
+}
+
+func (spanishBot) getGreeting() string {
+	return "Hola!"
+}
